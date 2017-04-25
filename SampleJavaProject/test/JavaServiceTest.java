b5517a0bb181ae10a32e1e692c5f972ad5fe4e7a
AB22766 <AB22766@CISCOW42LPC1314.ind.zensar.com>
2017-04-25 23:01:04 +0530

    Added JUnit

--- SampleJavaProject/test/JavaServiceTest.java
+++ SampleJavaProject/test/JavaServiceTest.java
@@ -1,8 +1,13 @@
+import org.junit.Test;
 
 public class JavaServiceTest  {
-	
+	@Test
 	public void testAdd(){
 		int result = new JavaService().add(2, 3);
 		assert 5==result;
+	}
+	@Test
+	public void testFail() {
+		assert 1 == 0;
 	}
 }
